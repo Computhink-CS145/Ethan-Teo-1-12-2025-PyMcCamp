c19b3f7e745441097550fa029d516b7f8b912065
Computhink-CS145 <cs145@computhink.com.sg>
2025-12-01 07:32:54 +0000

    Dec 1, 2025, 7:32 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -9,33 +9,45 @@ print("hello from day1")
 
 ########################################################################
 # Task 2:
-
-
+print("hi my name is Ethan Teo I am 10 years old I am from Ai Tong School")
+print("If I had $2000 as a reward,I would buy a car")
 
 ########################################################################
 # Task 3:
-
-
-
+animal1="tiger"
+animal2="lion"
+animal3="hippototomus"
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+num1=3
+num2=5
+print(num1+num2)
+print(num1*num2)
+print(num1/num2)
 ########################################################################
 # Task 5:
-
-
-
+name="dog"
+food="bone"
+print("my name is " +name + " I like to eat " + food)
 ########################################################################
 # Task 6:
+def info():
+    print("my name is Ethan")
+    print("I am 10 years old")
+    print("my school is Ai tong School")
+    print("If i had $2000 as a reward,I would buy a car")
 
-
-
+info()
 ########################################################################
 # Task 7:
+def addition(num1,num2):
+    print(num1+num2)
 
-
-
+addition(2,3)
+addition(243,236)
+addition(5000,3)
 ########################################################################
 # Additional exercises:
