dff7711150fbd70d348c4d0181eed32d1f309b9a
Computhink-CS145 <cs145@computhink.com.sg>
2025-12-01 08:55:01 +0000

    Dec 1, 2025, 8:55 AM

--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,45 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 ################# Function Section #############################
+def teleport():
+    agent.teleport_to_player()
+def fw():
+    agent.move(FORWARD,1)
+def bk():
+    agent.move(BACK,1)
+def up():
+    agent.move(UP,1)
+def down():
+    agent.move(DOWN,1)
+def tl():
+    agent.turn(LEFT)
+def tr():
+     agent.turn(RIGHT)
+
+player.on_chat("come",teleport)
+player.on_chat("fw",fw)
+player.on_chat("bk",bk)
+player.on_chat("down",down)
+player.on_chat("tl",tl)
+player.on_chat("tr",tr)
+player.on_chat("up",up)
+
+def obby1():
+    agent.move(FORWARD,4)
+    agent.move(LEFT,4)
+    agent.move(FORWARD,3)
+    agent.move(UP, 1)
+    agent.move(FORWARD,1)
+    agent.move(UP, 1)
+    agent.move(FORWARD,1)
+    agent.move(UP, 1)
+    agent.move(FORWARD,3)
+    agent.move(DOWN,1)
+    agent.move(FORWARD,1)
+    agent.move(DOWN,1)
+    agent.move(FORWARD,1) 
+    agent.move(DOWN,1)
+    agent.move(FORWARD,1)
 
 
 ################## On Chat Commands Section #####################
