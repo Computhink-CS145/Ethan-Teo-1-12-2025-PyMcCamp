a69f9e2fa95e32d1d49ea867cd71afb1505847d7
Computhink-CS145 <cs145@computhink.com.sg>
2025-12-03 08:52:15 +0000

    Dec 3, 2025, 8:52 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -3,43 +3,56 @@
 print("hello from day3")
 
 ########################################################################
-# Task 1:
-
-
-
-########################################################################
-# Task 2:
-
-
-
-########################################################################
-# Task 3:
-
-
-
+# # Task 1:
+# #Print("mytitle" + "myname" + "orders to destroy a fotress")
+# ########################################################################
+# # Task 2:
+# name = "Ethan"
+# num_pens = 123
+# print(name + "bought "+ str(num_pens))
+# ########################################################################
+# # Task 3:
+# num1 = int(input("your first number is?"))
+# num2 = int(input("your second number is?"))
+# sum = num1 + num2 
+# print(str(num1) + "+"+ str(num2) + "=" +str(sum))
 ########################################################################
 # Task 4:
-
-
+# price = 5.00
+# amt_of_item = int(input("how much item do you need?"))
+# total_cost = price*amt_of_item
+# print("the total cost is "+ str(total_cost))
 
 ########################################################################
 # Task 5:
-
-
-
+# age1=int(input("what is age1"))
+# age2=int(input("what is age2"))
+# if age1 == age2:
+#     print("age1 and age2 are the same age")
+# else:
+#     print("age1 is younger than age2")
 ########################################################################
 # Task 6:
-
-
+# password="passme"
+# guess= input("what is the password")
+# if guess == password:
+#     print("acess granted")
+# else:
+#     print("acess denied")
 
 ########################################################################
 # Task 7:
-
-
-
+import random
+# for count in range(10):
+#     a=random.randint(10,100)
+#     print(a)
 ########################################################################
 # Task 8:
-
+num1=random.randint(10,1000)
+num2=random.randint(10,1000)
+sum = num1 + num2
+result = int
+input("num1 + num2")
 
 
 ########################################################################
